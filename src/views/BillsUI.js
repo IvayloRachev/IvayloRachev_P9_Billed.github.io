13ff65633619eb43c8d742738b7374cad52ee0b8
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-12-29 14:14:53 +0100

    bug in Bills.js resolved
    Le bug du test Bills.js corrigé

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,7 +19,9 @@ const row = (bill) => {
     `)
   }
 
+//Ajout de la méthode sort pour le tri des dates
 const rows = (data) => {
+  data?.sort((a, b) => (a.originalDate || a.date) < (b.originalDate || b.date) ? 1 : -1);
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
