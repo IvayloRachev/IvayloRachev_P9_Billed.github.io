1d73a9a463f2805dde7aa1ccf4b71f797ed9d334
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2023-01-02 10:41:01 +0100

    Add tests for mail icon and newBill form
    Ajout des tests pour l'icone du mail et pour le formulaire de nouvelle note de frais

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -15,10 +15,49 @@ jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then mail icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const mailIcon = screen.getByTestId("icon-mail");
+
+      expect(mailIcon.className).toBe("active-icon");
+    });
+  });
+  describe("When I select a file", () => {
+    test("Verify if handleChangeFile have been called", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      document.body.innerHTML = NewBillUI();
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const inputBtn = screen.getByTestId("file");
+      inputBtn.addEventListener("change", handleChangeFile);
+      fireEvent.change(inputBtn, {
+        target: {
+          files: [new File(["test.jpg"], "test.jpg", { type: "image/jpg" })],
+        },
+      });
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(inputBtn.files[0].name).toBe("test.jpg");
+    });
+  });
+});
