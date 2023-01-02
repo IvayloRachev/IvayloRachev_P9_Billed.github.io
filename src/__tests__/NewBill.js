b0c9c36aa700808894e19b2ccb1e91d26d326fbd
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2023-01-02 10:32:07 +0100

    Add import
    Ajout des import

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,10 +1,16 @@
 /**
  * @jest-environment jsdom
  */
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import BillsUI from "../views/BillsUI.js";
+import NewBill from "../containers/NewBill.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router.js";
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+jest.mock("../app/store", () => mockStore);
 
 
 describe("Given I am connected as an employee", () => {
